00c5368716502f161393053a3ebcfccaf7f1a978
Donatas Pocius <pocius.coding@gmail.com>
2022-08-23 20:45:46 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+let imgs = [
+  {
+    src: "https://images.unsplash.com/photo-1615195627275-48660e9cd84f",
+    alt: "nebula cloud",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1570032257806-7272438f38da",
+    alt: "mountains reflection on water",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1525054098605-8e762c017741",
+    alt: "strong waves",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1562207520-19c0ebd8264f",
+    alt: "green mountains and blue sky",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1617191519105-d07b98b10de6",
+    alt: "blue and purple galaxy",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1441829266145-6d4bfbd38eb4",
+    alt: "blue wavy water",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1615114814213-a245ffc79e9a",
+    alt: "brown and black galaxy",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1552604660-a8c4dde15b2e",
+    alt: "person on clif in canyon",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1564295644023-16f14ac50b93",
+    alt: "whale tale above water surface",
+  },
+];
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  imgs.forEach((img) => 
+  document.createElement("img");
+  
+  );
+});
